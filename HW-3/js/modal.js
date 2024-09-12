b541b33ae389e9640cdbf84101ec07f53b93292d
Alaa-M16 <152276465+Alaa-M16@users.noreply.github.com>
2024-09-12 13:22:23 +0600

    Update modal.js

--- HW-3/js/modal.js
+++ HW-3/js/modal.js
@@ -1,42 +1,25 @@
-//MODAL
-
-const modal = document.querySelector('.modal')
-const modalTriggerButton = document.querySelector('#btn-get')
-const modalCloseButton = document.querySelector('.modal_close')
-
+// MODAL
+const modal = document.querySelector('.modal');
+const modalTriggerButton = document.querySelector('#btn-get');
+const modalCloseButton = document.querySelector('.modal_close');
 
 const openModal = () => {
-    modal.style.display = 'block'
-    document.body.style.overflow = 'hidden'
-    clearInterval(setTimerModal)
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    clearTimeout(setTimerModal); 
 }
 
 const closeModal = () => {
-    modal.style.display = 'none'
-    document.body.style.overflow = ''
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
 }
 
-modalTriggerButton.onclick = () => openModal()
-modalCloseButton.onclick = () => closeModal()
+modalTriggerButton.onclick = () => openModal();
+modalCloseButton.onclick = () => closeModal();
 modal.onclick = (event) => {
-    if (event.target === modal){
-        closeModal()
+    if (event.target === modal) {
+        closeModal();
     }
 }
 
-const setTimerModal = setTimeout(openModal, 10000)
-
-const ScrollToBottom = () => {
-    return window.innerHeight + window.scrollY + 10>= document.body.offsetHeight;
-}
-const scrollHandler = () => {
-    if (ScrollToBottom()) {
-        openModal()
-    }
-}
-window.addEventListener('scroll',()=>{ scrollHandler()
-    console.log()
-});
-
-
-// removeEventListener()
+const setTimerModal = setTimeout(openModal, 10000);
