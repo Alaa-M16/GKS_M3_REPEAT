9154ed64eb0bc50f64c5fb4ad5779950bf6bdef2
Alaa-M16 <152276465+Alaa-M16@users.noreply.github.com>
2024-09-19 15:25:57 +0600

    modal.js

--- HW-4/js/modal.js
+++ HW-4/js/modal.js
@@ -4,10 +4,13 @@ const modal = document.querySelector(".modal");
 const modalTriggerButton = document.querySelector("#btn-get");
 const modalCloseButton = document.querySelector(".modal_close");
 
+let hasModalOpenedByScroll = false;
+
 const openModal = () => {
   modal.style.display = "block";
   document.body.style.overflow = "hidden";
-  clearTimeout(setTimerModal);
+  clearInterval(setTimerModal);
+  removeScrollEventListener();
 };
 
 const closeModal = () => {
@@ -16,7 +19,9 @@ const closeModal = () => {
 };
 
 modalTriggerButton.onclick = () => openModal();
+
 modalCloseButton.onclick = () => closeModal();
+
 modal.onclick = (event) => {
   if (event.target === modal) {
     closeModal();
@@ -24,3 +29,20 @@ modal.onclick = (event) => {
 };
 
 const setTimerModal = setTimeout(openModal, 10000);
+
+const isBottomOfPage = () => {
+  return window.innerHeight + window.scrollY + 10 >= document.body.offsetHeight;
+};
+
+const scrollHandler = () => {
+  if (isBottomOfPage() && !hasModalOpenedByScroll) {
+    openModal();
+    hasModalOpenedByScroll = true;
+  }
+};
+
+window.addEventListener("scroll", scrollHandler);
+
+const removeScrollEventListener = () => {
+  window.removeEventListener("scroll", scrollHandler);
+};
