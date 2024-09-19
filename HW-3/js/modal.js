6aa23dd0eccae65fe9954be951bbf07130473d4a
Alaa-M16 <152276465+Alaa-M16@users.noreply.github.com>
2024-09-19 15:06:30 +0600

    modal.js

--- HW-3/js/modal.js
+++ HW-3/js/modal.js
@@ -1,25 +1,48 @@
 // MODAL
-const modal = document.querySelector('.modal');
-const modalTriggerButton = document.querySelector('#btn-get');
-const modalCloseButton = document.querySelector('.modal_close');
+
+const modal = document.querySelector(".modal");
+const modalTriggerButton = document.querySelector("#btn-get");
+const modalCloseButton = document.querySelector(".modal_close");
+
+let hasModalOpenedByScroll = false;
 
 const openModal = () => {
-    modal.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-    clearTimeout(setTimerModal); 
-}
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+  clearInterval(setTimerModal);
+  removeScrollEventListener();
+};
 
 const closeModal = () => {
-    modal.style.display = 'none';
-    document.body.style.overflow = '';
-}
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+};
 
 modalTriggerButton.onclick = () => openModal();
+
 modalCloseButton.onclick = () => closeModal();
+
 modal.onclick = (event) => {
-    if (event.target === modal) {
-        closeModal();
-    }
-}
+  if (event.target === modal) {
+    closeModal();
+  }
+};
 
 const setTimerModal = setTimeout(openModal, 10000);
+
+const isBottomOfPage = () => {
+  return window.innerHeight + window.scrollY + 10 >= document.body.offsetHeight;
+};
+
+const scrollHandler = () => {
+  if (isBottomOfPage() && !hasModalOpenedByScroll) {
+    openModal();
+    hasModalOpenedByScroll = true;
+  }
+};
+
+window.addEventListener("scroll", scrollHandler);
+
+const removeScrollEventListener = () => {
+  window.removeEventListener("scroll", scrollHandler);
+};
